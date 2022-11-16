e350b9f0cc16c7f2584f01abc427df8455d49c2d
Dan Stein <dan.stein@generalbioinformatics.com>
2022-11-16 23:25:40 +0000

    first

---
+++ src/schema.rs
@@ -0,0 +1,65 @@
+use juniper::{EmptySubscription, FieldResult, RootNode};
+
+#[derive(GraphQLEnum)]
+enum Episode {
+    NewHope,
+    Empire,
+    Jedi,
+}
+
+use juniper::{GraphQLEnum, GraphQLInputObject, GraphQLObject};
+
+#[derive(GraphQLObject)]
+#[graphql(description = "A humanoid creature in the Star Wars universe")]
+struct Human {
+    id: String,
+    name: String,
+    appears_in: Vec<Episode>,
+    home_planet: String,
+    age: i32,
+}
+
+#[derive(GraphQLInputObject)]
+#[graphql(description = "A humanoid creature in the Star Wars universe")]
+struct NewHuman {
+    name: String,
+    appears_in: Vec<Episode>,
+    home_planet: String,
+    age: i32,
+}
+
+pub struct QueryRoot;
+
+#[juniper::graphql_object]
+impl QueryRoot {
+    fn human(_id: String) -> FieldResult<Human> {
+        Ok(Human {
+            id: "1234".to_owned(),
+            name: "Luke".to_owned(),
+            appears_in: vec![Episode::NewHope],
+            home_planet: "Mars".to_owned(),
+            age: 43
+        })
+    }
+}
+
+pub struct MutationRoot;
+
+#[juniper::graphql_object]
+impl MutationRoot {
+    fn create_human(new_human: NewHuman) -> FieldResult<Human> {
+        Ok(Human {
+            id: "1234".to_owned(),
+            name: new_human.name,
+            appears_in: new_human.appears_in,
+            home_planet: new_human.home_planet,
+            age: new_human.age,
+        })
+    }
+}
+
+pub type Schema = RootNode<'static, QueryRoot, MutationRoot, EmptySubscription>;
+
+pub fn create_schema() -> Schema {
+    Schema::new(QueryRoot {}, MutationRoot {}, EmptySubscription::new())
+}
