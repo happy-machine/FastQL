fe24e4eb30d399ae9254eccc4a90cbe8ffe37087
Dan Stein <dan.stein@generalbioinformatics.com>
2022-11-17 00:07:41 +0000

    ..

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,45 @@
+use actix_cors::Cors;
+use actix_web::{
+    get, middleware, route,
+    web::{self, Data},
+    App, HttpRequest, HttpResponse, HttpServer, Responder,
+};
+use actix_web_lab::respond::Html;
+use juniper::http::{graphiql::graphiql_source, GraphQLRequest};
+use juniper::{EmptySubscription, FieldResult, RootNode};
 use pyo3::prelude::*;
 use std::cell::Cell;
-use std::{io, sync::Arc};
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Mutex;
 use std::thread;
-use actix_cors::Cors;
-use actix_web::{get, middleware, route, web::{self, Data}, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use actix_web_lab::respond::Html;
-use juniper::http::{graphiql::graphiql_source, GraphQLRequest};
-
+use std::{io, sync::Arc};
 static GLOBAL_DATA: Mutex<Vec<i32>> = Mutex::new(Vec::new());
 
 mod schema;
-use crate::schema::{create_schema, Schema};
+use crate::schema::{Episode, Human, MutationRoot};
+
+pub struct QueryRoot;
+
+#[juniper::graphql_object]
+impl QueryRoot {
+    fn human(_id: String) -> FieldResult<Human> {
+        Ok(Human {
+            id: "1234".to_owned(),
+            name: "Luke".to_owned(),
+            appears_in: GLOBAL_DATA.lock().unwrap().to_owned(),
+            home_planet: "Mars".to_owned(),
+            age: 43,
+        })
+    }
+}
+
+type Schema = RootNode<'static, QueryRoot, MutationRoot, EmptySubscription>;
+
+fn create_schema() -> Schema {
+    Schema::new(QueryRoot {}, MutationRoot {}, EmptySubscription::new())
+}
 /// simple handle
-fn wrapper(){
+fn wrapper() {
     // struct Memo {
     //     value: String
     // }
@@ -31,49 +56,48 @@ fn wrapper(){
     //     value: "".to_string()
     // };
     // static mut shared:String;
- 
-#[get("/graphiql")]
-async fn graphql_playground() -> impl Responder {
-    Html(graphiql_source("/graphql", None))
-}
 
-/// GraphQL endpoint
-#[route("/graphql", method = "GET", method = "POST")]
-async fn graphql(st: web::Data<Schema>, data: web::Json<GraphQLRequest>) -> impl Responder {
-    let user = data.execute(&st, &()).await;
-    // match serde_json::to_string(&user.serialize()) {
-    //     Ok(result) => query = result,
-    //     _ => {
-    //         //
-    //     }
-    // }
-    // println!("STUFFFFFFFF {:?}", user.get_field_value("age").unwrap().as_string_value().unwrap());
-    GLOBAL_DATA.lock().unwrap().push(42);
-    HttpResponse::Ok().json(user)
-}
+    #[get("/graphiql")]
+    async fn graphql_playground() -> impl Responder {
+        Html(graphiql_source("/graphql", None))
+    }
 
-#[actix_web::main]
-async fn main() -> io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info");
-    env_logger::init();
-    let schema = Arc::new(create_schema());
-    // Create some global state prior to building the server
-    #[allow(clippy::mutex_atomic)] // it's intentional.
-    let counter1 = web::Data::new(Mutex::new(0usize));
-    let counter3 = web::Data::new(AtomicUsize::new(0usize));
+    /// GraphQL endpoint
+    #[route("/graphql", method = "GET", method = "POST")]
+    async fn graphql(st: web::Data<Schema>, data: web::Json<GraphQLRequest>) -> impl Responder {
+        let user = data.execute(&st, &()).await;
+        // match serde_json::to_string(&user.serialize()) {
+        //     Ok(result) => query = result,
+        //     _ => {
+        //         //
+        //     }
+        // }
+        // println!("STUFFFFFFFF {:?}", user.get_field_value("age").unwrap().as_string_value().unwrap());
+        HttpResponse::Ok().json(user)
+    }
 
-    // move is necessary to give closure below ownership of counter1
-    HttpServer::new(move || {
-        // Create some thread-local state
-        let counter2 = Cell::new(0u32);
+    #[actix_web::main]
+    async fn main() -> io::Result<()> {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+        env_logger::init();
+        let schema = Arc::new(create_schema());
+        // Create some global state prior to building the server
+        #[allow(clippy::mutex_atomic)] // it's intentional.
+        let counter1 = web::Data::new(Mutex::new(0usize));
+        let counter3 = web::Data::new(AtomicUsize::new(0usize));
 
-        App::new()
-            .app_data(Data::from(schema.clone()))
-            .service(graphql)
-            .service(graphql_playground)
-            // the graphiql UI requires CORS to be enabled
-            .wrap(Cors::permissive())
-            .wrap(middleware::Logger::default())
+        // move is necessary to give closure below ownership of counter1
+        HttpServer::new(move || {
+            // Create some thread-local state
+            let counter2 = Cell::new(0u32);
+
+            App::new()
+                .app_data(Data::from(schema.clone()))
+                .service(graphql)
+                .service(graphql_playground)
+                // the graphiql UI requires CORS to be enabled
+                .wrap(Cors::permissive())
+                .wrap(middleware::Logger::default())
             // .app_data(counter1.clone()) // add shared state
             // .app_data(counter3.clone()) // add shared state
             // .data(counter2) // add thread-local state
@@ -81,17 +105,17 @@ async fn main() -> io::Result<()> {
             // .wrap(middleware::Logger::default())
             // // register simple handler
             // .service(web::resource("/").to(index))
-    })
-    .workers(2)
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
-}
-
+        })
+        .workers(2)
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+    }
     #[pyfunction]
-    fn init(a: usize) -> PyResult<String> {
+    fn init(value: i32) -> PyResult<String> {
+        GLOBAL_DATA.lock().unwrap().push(value);
         thread::spawn(move || main());
-        Ok((a * 2).to_string())
+        Ok("GQLwrapper initialised...".to_string())
     }
 
     #[pyfunction]
--- src/schema.rs
+++ src/schema.rs
@@ -1,7 +1,7 @@
 use juniper::{EmptySubscription, FieldResult, RootNode};
 
 #[derive(GraphQLEnum)]
-enum Episode {
+pub enum Episode {
     NewHope,
     Empire,
     Jedi,
@@ -11,36 +11,21 @@ use juniper::{GraphQLEnum, GraphQLInputObject, GraphQLObject};
 
 #[derive(GraphQLObject)]
 #[graphql(description = "A humanoid creature in the Star Wars universe")]
-struct Human {
-    id: String,
-    name: String,
-    appears_in: Vec<Episode>,
-    home_planet: String,
-    age: i32,
+pub struct Human {
+    pub id: String,
+    pub name: String,
+    pub appears_in: Vec<i32>,
+    pub home_planet: String,
+    pub age: i32,
 }
 
 #[derive(GraphQLInputObject)]
 #[graphql(description = "A humanoid creature in the Star Wars universe")]
 struct NewHuman {
     name: String,
-    appears_in: Vec<Episode>,
+    appears_in: Vec<i32>,
     home_planet: String,
     age: i32,
-}
-
-pub struct QueryRoot;
-
-#[juniper::graphql_object]
-impl QueryRoot {
-    fn human(_id: String) -> FieldResult<Human> {
-        Ok(Human {
-            id: "1234".to_owned(),
-            name: "Luke".to_owned(),
-            appears_in: vec![Episode::NewHope],
-            home_planet: "Mars".to_owned(),
-            age: 43
-        })
-    }
 }
 
 pub struct MutationRoot;
@@ -56,10 +41,4 @@ impl MutationRoot {
             age: new_human.age,
         })
     }
-}
-
-pub type Schema = RootNode<'static, QueryRoot, MutationRoot, EmptySubscription>;
-
-pub fn create_schema() -> Schema {
-    Schema::new(QueryRoot {}, MutationRoot {}, EmptySubscription::new())
 }
