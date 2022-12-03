98fbd503f0f48775ce00d2ba874775fcd417864b
Dan Stein <dan.stein@generalbioinformatics.com>
2022-12-03 21:16:39 +0000

    trying to mix python with rust

--- fastqlapi/__init__.py
+++ fastqlapi/__init__.py
@@ -1,5 +1,5 @@
 # re-export rust myproj module at this level
-from .fastqlapi import *
+from fastqlapi import *
 
 # export vanilla_python.py functions as vanilla_python module
 from . import wrapper
