a3447fcc79083082ba9106831709860c72c059dd
Dan Stein <dan.stein@generalbioinformatics.com>
2022-12-04 03:19:11 +0000

    0.1.3 - fixes env issues

--- fastqlapi/__init__.py
+++ fastqlapi/__init__.py
@@ -1,6 +1,6 @@
 # re-export rust myproj module at this level
-from .fastqlapi import *
+from fastqlapi import *
 
 # export vanilla_python.py functions as vanilla_python module
-from .wrapper import fastql_server
+from . import server
 from .test_schema import test_args, test_fields
