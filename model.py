e802a354e46e602909e97ccd3af0e19e77630e9e
Dan Stein <dan.stein@generalbioinformatics.com>
2022-12-04 14:06:09 +0000

    trying to build for linux

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
-from fastqlapi import wrapper, test_schema
+from fastqlapi import fastql_server
 def test(**kwargs):
     print (kwargs['input'])
     return {
         'output': "test response",
     }
 
-wrapper.fastql_server.start(callback=test, args={"input": { "type": "String"}}, fields={"output": { "type": "String"}})
+fastql_server.start(callback=test, args={"input": { "type": "String"}}, fields={"output": { "type": "String"}})
