851b8c632416b3343888f9352b2092a0b12fc9ae
Dan Stein <dan.stein@generalbioinformatics.com>
2022-12-18 10:05:40 +0000

    0.3.0

--- python/fastqlapi/__init__.py
+++ python/fastqlapi/__init__.py
@@ -1,6 +1,6 @@
 # re-export rust myproj module at this level
-from fastqlapi import *
+from .fastqlapi import *
 
 # export vanilla_python.py functions as vanilla_python module
-from .api import fastql_server
+from .server import fastql_server
 from .test_schema import test_args, test_fields
